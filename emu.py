ea768062d0031b51db953c67e50a032368bb2d33
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2021-11-18 10:58:55 +0100

    emulator: emulate more stuff:
    * encoder config command
    * encoder position command
    * target position command

--- emu.py
+++ emu.py
@@ -1,32 +1,94 @@
 #!/usr/bin/env python
+import time
+from typing import Optional, List
 import re
 import socket
-from threading import Thread
+from threading import Thread, Lock
+from dataclasses import dataclass
 
-CHANNELS = 4
 HOST = "127.0.0.1"
 PORT = 4001
 
+CHANNELS = 3
 
-COMMAND_RE = re.compile(r"^X(\?|\d*)(.*)")
+
+class Channel:
+    def __init__(self, encoder_position: int, target_position: int):
+        self.encoder_position = encoder_position
+        self.target_position = target_position
+        self._lock = Lock()
+
+    def __enter__(self):
+        self._lock.acquire()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self._lock.release()
 
 
-def _parse_command(command):
+@dataclass
+class Controller:
+    channels: List[Channel]
+
+
+@dataclass
+class Command:
+    channel: str
+    name: str
+    argument: Optional[str]
+    suppress_response: bool
+
+
+COMMAND_RE = re.compile(r"^X(\?|\d*)([ETY].*)?([;\n\r])$")
+
+
+def motor_ticker(controller: Controller):
+    def move_motor(channel):
+        if channel.target_position == channel.encoder_position:
+            # at target position, no need to move anything
+            return
+        delta = 1 if channel.target_position > channel.encoder_position else -1
+        channel.encoder_position += delta
+
+    while True:
+        for channel in controller.channels:
+            move_motor(channel)
+        time.sleep(1)
+
+
+def _parse_command(command) -> Optional[Command]:
     print(f"parse {command=}")
     res = COMMAND_RE.match(command)
     if res is None:
         print(f"no match for {command=}")
-        return
+        return None
 
-    channel, command = res.groups()
-    print(f"{channel=} {command=}")
+    channel, command, delimiter = res.groups()
 
-    return channel, command
+    argument = None
+    if command is not None and "," in command:
+        command, argument = command.split(",")
+
+    suppress_response = delimiter == ";"
+
+    return Command(channel, command, argument, suppress_response)
 
 
-def _read_line(sock) -> str:
+def _read_command_str(sock) -> str:
+    def endswith_delimiter():
+        if line.endswith(b"\n"):
+            return True
+
+        if line.endswith(b"\r"):
+            return True
+
+        if line.endswith(b";"):
+            return True
+
+        return False
+
     line = b""
-    while not line.endswith(b"\n"):
+    while not endswith_delimiter():
         res = sock.recv(4096)
         if res == b"":
             break
@@ -41,58 +103,93 @@ def _list_channels_cmd():
         for n in range(CHANNELS):
             yield f"X{n}\n"
 
-    #        yield f"Yhaha\n"
-
     reply = "".join(gen_channels())
     return reply.encode()
 
 
-def _encode_position_cmd(channel):
-    return f"X{channel}E:0\r".encode()
+def _target_cmd(command: Command, controller: Controller) -> bytes:
+    if len(command.name) == 1:
+        # this is 'read target position' version of the command
+        with controller.channels[int(command.channel)] as channel:
+            position = channel.target_position
+
+        return f"X{command.channel}T:{position}\r".encode()
+
+    new_target_pos = int(command.name[1:])
+    with controller.channels[int(command.channel)] as channel:
+        old_position = channel.target_position
+        channel.target_position = new_target_pos
+
+    return f"X{command.channel}T:{old_position}\r".encode()
 
 
-def _handle_command(line):
-    res = _parse_command(line)
-    if res is None:
-        return b"parse error\n"
+def _encode_position_cmd(command: Command, controller: Controller):
+    with controller.channels[int(command.channel)] as channel:
+        position = channel.encoder_position
 
-    channel, command = res
-
-    if channel == "?":
-        return b"X?:PMD401 V18-emu\r"
-
-    if channel == "127" and command == "":
-        return _list_channels_cmd()
-
-    if command == "E":
-        return _encode_position_cmd(channel)
-
-    return b"wat?\n"
-
-    # command = command.decode()
-    #
-    # if command == b"X?":
-    #     return b"X?:PMD401 V18-emu\r"
-    #
-    # if command == b"X127":
-    #     return _list_channels_cmd()
-    #
-    # return b"wat?"
+    return f"X{command.channel}E:{position}\r".encode()
 
 
-def serve_client(connection, client):
+def _config_encoder_cmd(command: Command):
+    reply = f"X{command.channel}Y13"
+
+    if command.argument is None:
+        reply += ":1, Quad_32\r"
+    else:
+        reply += f",{command.argument}\n"
+
+    return reply.encode()
+
+
+def _handle_command(connection, line, controller: Controller):
+    command = _parse_command(line)
+    print(f"{command=}")
+
+    if command is None:
+        reply = b"parse error\n"
+    elif command.channel == "?":
+        reply = b"X?:PMD401 V18-emu\r"
+    elif command.channel == "127" and command.name is None:
+        reply = _list_channels_cmd()
+    elif command.name[0] == "T":
+        reply = _target_cmd(command, controller)
+    elif command.name == "E":
+        reply = _encode_position_cmd(command, controller)
+    elif command.name == "Y13":
+        reply = _config_encoder_cmd(command)
+    else:
+        reply = b"wat?\n"
+
+    if command is None or (not command.suppress_response):
+        connection.sendall(reply)
+
+
+def serve_client(connection, client, controller: Controller):
     try:
-        while (line := _read_line(connection)) != "":
-            print(f"{line=}")
-            reply = _handle_command(line[:-1])
-            connection.sendall(reply)
+        while (line := _read_command_str(connection)) != "":
+            _handle_command(connection, line, controller)
     except BrokenPipeError:
         print("connection closed, I guess")
+
+    connection.shutdown(socket.SHUT_WR)
+    connection.close()
 
     print(f"bye {client}")
 
 
+def get_controller():
+    def get_channels():
+        for n in range(CHANNELS):
+            yield Channel(0, 0)
+
+    return Controller(list(get_channels()))
+
+
 def main():
+    controller = get_controller()
+
+    Thread(target=motor_ticker, args=[controller]).start()
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind((HOST, PORT))
     sock.listen()
@@ -100,7 +197,7 @@ def main():
     while True:
         print(f"Waiting for connection on {HOST}:{PORT}")
         connection, client = sock.accept()
-        Thread(target=serve_client, args=[connection, client]).start()
+        Thread(target=serve_client, args=[connection, client, controller]).start()
 
 
 main()
